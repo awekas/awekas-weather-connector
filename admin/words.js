2cd423f03ba36e41875365a4df98987a5b889918
Othmar Gattringer <mail@awekas.at>
2023-08-21 16:29:25 +0200

    erste Testversion online

---
+++ admin/words.js
@@ -0,0 +1,45 @@
+/*global systemDictionary:true */
+"use strict";
+
+systemDictionary = {
+  "adapter settings": {
+    en: "Adapter settings for AWEKAS data collector",
+    de: "Adaptereinstellungen für AWEKAS Datensammler",
+    ru: "Параметры адаптера для коллектора данных AWEKAS",
+    pt: "Configurações do adaptador para o coletor de dados AWEKAS",
+    nl: "AWEKAS data verzamelaar",
+    fr: "Paramètres d'adaptateur pour le collecteur de données AWEKAS",
+    it: "Impostazioni adattatori per collettore dati AWEKAS",
+    es: "Ajustes de adaptador para recopilador de datos AWEKAS",
+    pl: "Adapter ustawienia dla kolekcjonerów danych AWEKAS",
+    uk: "Параметри адаптера для збору даних AWEKAS",
+    "zh-cn": "A. 非洲KAS数据收集机构的数据范围",
+  },
+  apiKey: {
+    en: "AWEKAS API key",
+    de: "AWEKAS API Schlüssel",
+    ru: "AWEKAS API ключ",
+    pt: "AWEKAS chave API",
+    nl: "AWEKAS API",
+    fr: "AWEKAS API key",
+    it: "AWEKAS chiave API",
+    es: "AWEKAS API key",
+    pl: "AWEKAS key (ang.)",
+    uk: "AWEKAS API ключ",
+    "zh-cn": "AWEKASAPI的关键",
+  },
+  keyHelp: {
+    en: "Use the AWEKAS API Token/Key from AWEKAS user settings. You can get it from https://my.awekas.at",
+    de: "Verwenden Sie die AWEKAS API Token/Key von den AWEKAS-Benutzereinstellungen. Sie können diesen unter https://my.awekas.at erhalten",
+    ru: "Используйте AWEKAS API Token/Key из настроек пользователя AWEKAS. Вы можете получить его из https://my.awekas.at",
+    pt: "Use o Token/Key API AWEKAS de configurações de usuário AWEKAS. Você pode obtê-lo a partir de https://my.awekas.at",
+    nl: "Gebruik de AWEKAS API Token/Key van AWEKAS gebruikers. Je kunt het van mijn aardappelen krijgen",
+    fr: "Utilisez le jeton de l'API AWEKAS/Key à partir des paramètres de l'utilisateur AWEKAS. Vous pouvez l ' obtenir à l ' adresse https://my.awekas.at",
+    it: "Utilizzare il Token/Key API AWEKAS dalle impostazioni utente AWEKAS. È possibile ottenere da https://my.awekas.at",
+    es: "Utilice el AWEKAS API Token/Key de la configuración de usuario de AWEKAS. Puede obtenerlo desde https://my.awekas.at",
+    pl: "AWEKAS API Token/Key from AWEKAS (ang.). Można ją dostać z https:/my.awekas.at",
+    uk: "Використовуйте AWEKAS API Token/Key з налаштуваннями користувача AWEKAS. Ви можете отримати його з https://my.awekas.at",
+    "zh-cn":
+      "AWEKAS API Token/Key从AKAS用户环境中使用。 你可以从http://my.awekas.at查阅。",
+  },
+};
